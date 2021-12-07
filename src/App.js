9e1d4054df81c75e2648cac1721a59c00b4508a1
Vitalii Semenenko <SemenenkoSVAT@gmail.com>
2021-12-08 00:26:24 +0200

    added TodoList

--- src/App.js
+++ src/App.js
@@ -4,26 +4,53 @@
 // import Container from './components/Container/Container';
 // import Box from './components/Box/Box';
 // import paintings from './paintings.json';
-import Counter from './components/Counter';
-import Dropdown from './components/Dropdown';
-import ColorPickerNew from './components/ColorPickerNew';
+// import Counter from './components/Counter';
+// import Dropdown from './components/Dropdown';
+// import ColorPickerNew from './components/ColorPickerNew';
+import React, { Component } from 'react';
+import TodoList from './components/TodoList';
+import initialTodos from './todos.json';
 
-const colorPickerOptions = [
-  { label: 'red', color: '#F44336' },
-  { label: 'green', color: '#4CAF50' },
-  { label: 'blue', color: '#2196F3' },
-  { label: 'grey', color: '#607D8B' },
-  { label: 'pink', color: '#E91E63' },
-  { label: 'indigo', color: '#3F51B5' },
-];
+// const colorPickerOptions = [
+//   { label: 'red', color: '#F44336' },
+//   { label: 'green', color: '#4CAF50' },
+//   { label: 'blue', color: '#2196F3' },
+//   { label: 'grey', color: '#607D8B' },
+//   { label: 'pink', color: '#E91E63' },
+//   { label: 'indigo', color: '#3F51B5' },
+// ];
 
-function App() {
-  return (
-    <>
-      <ColorPickerNew options={colorPickerOptions} />
-      <Dropdown />
-      <Counter initialValue={10} />
-      {/* <Container>
+class App extends Component {
+  state = {
+    todos: initialTodos,
+  };
+
+  deleteTodo = todoId => {
+    this.setState(prevState => ({
+      todos: prevState.todos.filter(todo => todo.id !== todoId),
+    }));
+  };
+
+  render() {
+    const { todos } = this.state;
+
+    const totalTodoCount = todos.length;
+    const completedTodoCount = todos.reduce(
+      (total, todo) => (todo.completed ? total + 1 : total),
+      0,
+    );
+
+    return (
+      <>
+        <div>
+          <p>Quantity: {totalTodoCount}</p>
+          <p>Done: {completedTodoCount}</p>
+        </div>
+        <TodoList todos={todos} onDeleteTodo={this.deleteTodo} />
+        {/* <ColorPickerNew options={colorPickerOptions} />
+        <Dropdown />
+        <Counter initialValue={10} /> */}
+        {/* <Container>
         <Box type="small" classNames="big red" styles={{ color: '#ffffff' }} />
         <Box type="medium" />
         <Box type="large" />
@@ -34,8 +61,9 @@ function App() {
         <ColorPicker options={colorPickerOptions} />
         <PaintingList items={paintings} />
       </Container> */}
-    </>
-  );
+      </>
+    );
+  }
 }
 
 export default App;
